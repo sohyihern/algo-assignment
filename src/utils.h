f088e61c934336988ee603c89075450449948429
yx <youxiangkoh1@gmail.com>
2026-06-18 17:09:00 +0800

    filled in member details

--- src/utils.h
+++ src/utils.h
@@ -4,10 +4,10 @@
 // Lecture Class: TC6L
 // Tutorial Class: T22L
 // Trimester: 2610
-// Member_1: ID | NAME | EMAIL | PHONE
-// Member_2: ID | NAME | EMAIL | PHONE
-// Member_3: ID | NAME | EMAIL | PHONE
-// Member_4: ID | NAME | EMAIL | PHONE
+// Member_1: 243UC246W1 | KOH YOU XIANG | KOH.YOU.XIANG@student.mmu.edu.my | 019-6581165
+// Member_2: 243UC246W0 | SOH YI HERN | SOH.YI.HERN@student.mmu.edu.my | 018-2991143
+// Member_3: 243UC246W3 | YAP JIET IN | YAP.JIET.IN@student.mmu.edu.my | 011-10991332
+// Member_4: 251UC250KN | PATRICK TOH TZY GUAN | PATRICK.TOH.TZY@student.mmu.edu.my | 0182086422
 // *********************************************************
 
 #ifndef UTILS_H
